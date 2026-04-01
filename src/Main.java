e416deb790ddf90c906dab999428a9e4aa14b05a
afifa-f <afifafatema@hotmail.com>
2026-04-01 16:41:23 +0100

    simple procedural script to log in and lock user out after 3 failed attempts

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+void main() {
+
+    //Procedural log in script
+    String userName = "CrazyCray";
+    String password = "HelloWorld!";
+
+    boolean loggedIn = false;
+    byte failedAttempts = 0;
+
+    //Log in loop will exit if correct password and username is entered or if wrong details have been entered 3 times
+    while (!loggedIn && failedAttempts < 3) {
+        IO.println("Enter username:");
+        String enteredUserName = IO.readln();
+
+        IO.println("Enter your password");
+        String enteredPassword = IO.readln();
+
+        if (enteredUserName.equals(userName) && enteredPassword.equals(password)) {
+            IO.println(userName + " has logged in");
+            loggedIn = true;
+            //
+        } else {
+            failedAttempts++; //tallies number of failed attempts
+            IO.println("Incorrect username or password!");
+            if (failedAttempts == 3) {
+                IO.println("LOCKED OUT");
+            }
+        }
+    }
+}
